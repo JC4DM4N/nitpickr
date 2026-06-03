74fc870c98a666cc80d05b173be3b02257ad18e1
james cadman <james_cadman@aol.co.uk>
2026-06-03 19:30:55 +0000

    updated authFetch to fix apps not loading

--- src/utils/authFetch.js
+++ src/utils/authFetch.js
@@ -1,5 +1,5 @@
 export async function authFetch(path, options = {}) {
-  const url = path.startsWith('/') ? `/api${path}` : path;
+  const url = path;
   const res = await fetch(url, options);
   if (res.status === 401) {
     window.dispatchEvent(new Event('auth:expired'));
